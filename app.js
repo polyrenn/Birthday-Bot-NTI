98d49e5cbee261083f4f30101f6ea82530cda72f
Niyi Enny <myjessy.sr@gmail.com>
2023-03-14 11:28:54 +0100

    Init

---
+++ app.js
@@ -0,0 +1,275 @@
+const puppeteer = require("puppeteer-core");
+const chromium = require("@sparticuz/chromium");
+const nodeHtmlToImage = require('node-html-to-image')
+const { WebClient } = require('@slack/web-api');
+const { GetObjectCommand, S3Client, PutObjectCommand} = require('@aws-sdk/client-s3');
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const fs = require('fs');
+
+
+const s3 = new S3Client({
+    credentials: {
+        accessKeyId: 'AKIA4OVWLAXW3TI64MWS',
+        secretAccessKey: '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',
+    }
+})
+
+async function displayTitle()  {
+    console.log('dhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+}
+
+const getUrl = async () => {
+    const params = {Bucket: 'birthday-designs', Key: 'my-object-key'};
+    const command = new GetObjectCommand(params);
+    const url = await getSignedUrl(s3, command);
+    console.log(url)
+}
+
+//getUrl()
+
+
+
+
+// Read the contents of the HTML file into a string
+const htmlString = fs.readFileSync('./app.html', 'utf8');
+
+
+
+// Print the HTML string to the console
+//console.log(htmlString);
+
+
+module.exports.handler = async (event) => {
+
+    //const objectKey = event.Records[0].s3.object.key;
+  
+    // Extract the file name from the object key
+    //const fileName = objectKey.split('/').pop();
+
+    const web = new WebClient('xoxb-4699850119287-4719021486260-Ds0sNpdtjqgpcT0Jge1hJvvr');
+
+    
+
+    const paramsEvent = {Bucket: 'slack-transformed-images', Key: event.key};
+    const commandEvent = new GetObjectCommand(paramsEvent);
+    const url = await getSignedUrl(s3, commandEvent);
+
+    const downloadResponse = await fetch(url, {
+        method: "GET",
+      });
+   
+
+    const arrayBuffer = await downloadResponse.arrayBuffer()
+      
+    const base64Image = await Buffer.from(arrayBuffer).toString('base64');
+      
+    
+
+    const cowrywiseLogo = await fs.readFileSync('./Vector.svg').toString('base64')
+    const base64Logo = await new Buffer.from(cowrywiseLogo)
+
+    const backgroundStreaks = await fs.readFileSync('./Group5.svg').toString('base64')
+    const base64Background = await new Buffer.from(backgroundStreaks)
+   
+   const image = await nodeHtmlToImage({
+    html: `<html>
+      <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+       body {
+          width: 1080px;
+          height: 1080px;
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+          background-color: blanchedalmond;
+          padding: 40px 60px;
+          position: relative;
+          background-image: url('data:image/svg+xml;base64,${base64Background}');
+          font-family: "Noto Color Emoji";
+        }
+        h3 {
+          margin: 0;
+      }
+      
+      p {
+          margin: 0;
+      }
+      
+      .top-section {
+          display: flex;
+      }
+      
+      .logo-wrap {
+          width: 100%;
+          display: flex;
+          justify-content: flex-end;
+      }
+      
+      .logo {
+          background-image: url('data:image/svg+xml;base64,${base64Logo}');
+          width: 56px;
+          height: 56px;
+          background-size: contain;
+      }
+      
+      .wrapper {
+        width: 100%;
+        height: 100%;
+    }
+      
+      .wrapper:before {
+          content: '';
+          display: block;
+          position: absolute;
+          left: 0;
+          top: 0;
+          width: 100%;
+          height: 100%;
+          opacity: 0.6;
+          z-index: 0;
+          background-repeat: no-repeat;
+          background-position: 50% 0;
+          background-size: contain
+        }
+        
+      
+      .header {
+          font-family: 'BR Firma';
+          color: #0066f5;
+          height: 10%;
+      }
+      
+      .confetti {
+          background-image: url('confetti.svg');
+          width: 56px;
+          height: 56px;
+          background-size: contain;
+      }
+      
+      .main-text {
+          margin: 0;
+          margin-bottom: 16px;
+          font-size: 64px;
+          font-weight: 600;
+      }
+      
+      .header-subtext {
+          font-size: 48px;
+      }
+      
+      .image-wrapper {
+          display: flex;
+          align-items: flex-end;
+          justify-content: space-between;
+          height: 70%;
+          flex-flow: row-reverse;
+          z-index: 1;
+      }
+      
+      .image {
+          width: 579px;
+          height: 579px;
+          background-image: url('data:image/jpeg;base64,${base64Image}');
+          background-size: cover;
+          border-radius: 500px 500px 500px 0;
+          background-repeat: no-repeat;
+      }
+      
+      .subtext {
+          font-size: 36px;
+          font-family: 'BR Firma';
+          font-weight: 400;
+          color: #0066f5
+      }
+        </style>
+      </head>
+      <body>
+      <div class="wrapper">
+          <div class="logo-wrap">
+              <div class="logo">
+  
+              </div>
+          </div>
+          
+          <div class="top-section">
+              <div class="header">
+                  <p class="main-text">Happy Birthday ${event.name} 🎉</p>
+                  <p class="header-subtext">Have a great year!</p>
+              </div>
+          </div>
+          
+  
+          <div class="image-wrapper">
+              <div class="image">
+            
+              </div>
+              <div class="subtext">
+                  <p>With love, <br/> Cowrywise.</p>
+              </div>
+          </div>
+       
+      </div>
+  </body>
+    </html>
+    `,
+    puppeteer: puppeteer,
+    puppeteerArgs: {
+        args: chromium.args,
+        executablePath: await chromium.executablePath()
+    },
+    beforeScreenshot: async (page) => {
+       await page.setViewport({
+            width: 1080,
+            height: 1080,
+            deviceScaleFactor: 1
+        });
+    }
+  })
+  console.log(image)
+  const params = {
+    Bucket: 'birthday-designs',
+    Key: 'my-object-key',
+    Body: image,
+    ContentType: 'image/jpeg'
+  };
+  
+  const command = new PutObjectCommand(params);
+  
+  const response = await s3.send(command);
+  console.log(response);
+  console.log(event)
+  const paramsGetSignedUrl = {Bucket: 'birthday-designs', Key: 'my-object-key'};
+  const commandGetSignedUrl = new GetObjectCommand(paramsGetSignedUrl);
+  const signedUrl = await getSignedUrl(s3, commandGetSignedUrl);
+  // Call the chat.postMessage method with the required parameters
+  //Here's Your Image It Expires Soon
+  const responsePostMessage = await web.chat.postMessage({
+    channel: 'C04LXD2JHLM',
+    blocks: [
+        {
+			"type": "section",
+			"text": {
+				"type": "plain_text",
+				"text": "Here's your Birthday Flyer"
+			}
+		},
+		{
+			"type": "image",
+			"title": {
+				"type": "plain_text",
+				"text": "Birthday Flyer",
+				"emoji": true
+			},
+			"image_url": `${signedUrl}`,
+			"alt_text": "Birthday Flyer"
+		},
+    ],
+ 
+    });
+  return signedUrl
+};
+  
+  
